9f7e61dc819c438c575d7c29de93ea913c5491f3
Isaac Hesslegrave <isaac.hesslegrave@gmail.com>
2025-12-03 22:40:01 +0000

    Update scripts

--- create-lang-dir.sh
+++ create-lang-dir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -u
 lang=$1
-mkdir -p "$lang/{2015..2024}/{01..25}"
-mkdir -p "$lang/2025/{01..12}"
+mkdir -p $lang/{2015..2025}/{01..24}
+mkdir -p $lang/2025/{01..25}
--- download-puzzle.sh
+++ download-puzzle.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -u
-lang=$1
-year=$2
-day=$3
-folder="$lang/$year/$day"
-curl "https://adventofcode.com/$year/day/$((10#$day))/input" -o "$folder/input" -b cookies.txt
-
+year=$1
+day=$2
+folder="$year/$day"
+curl "https://adventofcode.com/$year/day/$((10#$day))/input" -o "master/$folder/input" -b cookies.txt
+for i in $(find . -maxdepth 1 -type d -not -name ".*" -and -not -name "master")
+do
+	ln -fs "$(pwd)/master/${folder}/input" "$(pwd)/${i}/${folder}/input"
+done
