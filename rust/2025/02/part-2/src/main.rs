6d1ed6361be1df058c68aa38c0a9a42d68bc1d54
Isaac Hesslegrave <isaac.hesslegrave@gmail.com>
2025-12-07 15:27:20 +0000

    Add Day 2 part 2 solution

--- rust/2025/02/part-2/src/main.rs
+++ rust/2025/02/part-2/src/main.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 fn main() {
-    let file = File::open("../test").unwrap();
+    let file = File::open("../input").unwrap();
     let mut reader = BufReader::new(file);
     let mut invalid_ids: Vec<u64> = vec![];
     let mut input = String::new();
@@ -12,14 +12,15 @@ fn main() {
         let mut range = range.split('-');
         let start = range.next().unwrap().parse::<u64>().unwrap();
         let end = range.next().unwrap().parse::<u64>().unwrap();
-        for i in start..=end {
-            let str = i.to_string();
-            if str.len() % 2 == 1 {
-                continue;
-            }
-            let factor: u64 = 10_u64.pow(str.len() as u32 / 2) + 1;
-            if i % factor == 0 {
-                invalid_ids.push(i);
+        'outer: for i in start..=end {
+            for a in 1..=i.to_string().len()/2 {
+                if i.to_string().len() % a != 0 {
+                    continue;
+                }
+                if i.to_string().bytes().collect::<Vec<u8>>().windows(a).enumerate().filter(|(b,_)|b%a==0).map(|(_,b)|b).all(|s| *s == i.to_string().bytes().collect::<Vec<u8>>()[..a]) {
+                    invalid_ids.push(i);
+                    continue 'outer;
+                }
             }
         }
     }
